d7dc9fef21ed8766774ddc33314850c137da7713
Dianna-coder <diana270301@hotmail.com>
2020-11-04 08:59:01 -0300

    feat: add mais duas etapas do formulario de cliente

---
+++ src/screens/CadastroCliente/FormQuatro.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import colors from '../../styles/colors/index';
+
+import Header from '../../components/Header';
+import Input from '../../components/Input';
+import ImagePicker from '../../components/ImagePicker';
+import Button from '../../components/Button';
+
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+
+export default class FormQuatro extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      titulo: "",
+    };
+
+    this.handletituloChange = this.handletituloChange.bind(this);
+  }
+
+  handletituloChange = (value) => this.setState({ titulo: value });
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Header titulo='Defina Sua Senha' />
+
+        <ImagePicker
+        />
+
+        <Input
+          labelText='Senha  '
+          onChangeText={this.handletituloChange}
+          value={this.state.titulo}
+        />
+
+        <Input
+          labelText='Confirme Sua Senha'
+          onChangeText={this.handletituloChange}
+          value={this.state.titulo}
+        />
+
+        <Button titulo='CADASTRAR' />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.azulEscuro,
+    alignItems: 'center',
+  },
+  containerDoisInputs: {
+    flexDirection: 'row',
+    width: wp('79.71%'),
+    justifyContent: 'space-between',
+    alignItems: 'flex-end'
+  }
+});
---
+++ src/screens/CadastroCliente/FormTres.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import colors from '../../styles/colors/index';
+
+import Header from '../../components/Header';
+import Input from '../../components/Input';
+import ImagePicker from '../../components/ImagePicker';
+import Button from '../../components/Button';
+
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+
+export default class FormTres extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      titulo: "",
+    };
+
+    this.handletituloChange = this.handletituloChange.bind(this);
+  }
+
+  handletituloChange = (value) => this.setState({ titulo: value });
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Header titulo='Cadastre Seu Cartão' />
+
+        <ImagePicker
+        />
+
+        <Input
+          labelText='Titular'
+          onChangeText={this.handletituloChange}
+          value={this.state.titulo}
+        />
+
+        <Input
+          labelText='Número'
+          onChangeText={this.handletituloChange}
+          value={this.state.titulo}
+        />
+
+        <View style={styles.containerDoisInputs}>
+          <Input
+            withi={wp('35%')}
+            labelText='Data'
+            onChangeText={this.handletituloChange}
+            value={this.state.titulo}
+          />
+
+          <Input
+            withi={wp('35%')}
+            labelText='CVC'
+            onChangeText={this.handletituloChange}
+            value={this.state.titulo}
+          />
+        </View>
+
+        <Button />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.azulEscuro,
+    alignItems: 'center',
+  },
+  containerDoisInputs: {
+    flexDirection: 'row',
+    width: wp('79.71%'),
+    justifyContent: 'space-between',
+    alignItems: 'flex-end'
+  }
+});
