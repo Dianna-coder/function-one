669cc5246d8ff9ebf54c6d2670a9a4cb4d98f15c
Dianna-coder <diana270301@hotmail.com>
2020-11-04 08:57:52 -0300

    feat: criar componente para o checkbox

--- App.js
+++ App.js
@@ -5,20 +5,25 @@ import { StatusBar } from 'expo-status-bar';
 import colors from './src/styles/colors/index';
 
 // import FormUm from './src/screens/CadastroCliente/FormUm';
-import FormDois from './src/screens/CadastroCliente/FormDois';
+// import FormDois from './src/screens/CadastroCliente/FormDois';
+// import FormTres from './src/screens/CadastroCliente/FormTres';
+import FormQuatro from './src/screens/CadastroCliente/FormQuatro';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <FormDois />
 
-      <StatusBar 
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FormQuatro />
+
+        <StatusBar 
           style='light'
           backgroundColor = { colors.azulEscuro }
           translucent = {false} 
-      />
-    </View>
-  );
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
