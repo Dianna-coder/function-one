732e7b600c3332987883044785a27e81b7429951
Dianna-coder <diana270301@hotmail.com>
2020-11-12 17:46:56 -0300

    refactor: arrumar o layout dos botões e textos.

--- src/screens/TransitionScreens/Apresentacao/ScreenTres.js
+++ src/screens/TransitionScreens/Apresentacao/ScreenTres.js
@@ -1,40 +1,77 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+import Header from '../../../components/Header';
+import colors from '../../../styles/colors';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 
 export default function ScreenTres(props) {
   return (
     <View style={styles.conatiner}>
-      <Text style={{ color: "#FFFFFF", fontSize: 30 }}>
+      <Header titulo='' funcao={() => props.navigation.navigate('Main')} />
+
+      <View style={styles.interno}>
+        <Text style={styles.titulo}>
         Melhor Custo-benefício
         </Text>
 
-      <Image source={require('../../../../assets/icons/cofrinho.png')}>
-      </Image>
+        <Image source={require('../../../../assets/icons/cofrinho.png')}></Image>
 
-      <Text style={{ color: "#FFFFFF", fontSize: 20, textAlign: "center" }}>
+        <Text style={styles.paragrafo}>
         Escolha o técnico que pode solucionar seu problema e que também atenda a seu orçamento.
         </Text>
 
-      <TouchableOpacity style={{ flexDirection: "row" }} onPress={() => props.navigation.navigate('CadastroCliente')}>
-        <Text style={{
-          fontSize: 20,
-          justifyContent: "center",
-          alignItems: "center",
-          paddingTop: 25,
-          marginLeft: 30
-        }}>
-          PRÓXIMO
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity  onPress={() => props.navigation.navigate('CadastroCliente')}>
+          <Text style={styles.btn}>
+            Próximo
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Image style={styles.icon} source={require('../../../../assets/icons/telaTres.png')}></Image>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   conatiner: {
     flex: 1,
-    justifyContent: "space-evenly",
     alignItems: "center",
-    backgroundColor: "#0C1C41",
+    backgroundColor: colors.azulEscuro,
   },
+  interno: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly"
+  },
+  titulo: {
+    color: colors.branco,
+    fontSize: 30,
+    textAlign: "center",
+  },
+  paragrafo: {
+    width: wp ('88%'),
+    color: "#FFFFFF", 
+    fontSize: 18,
+    textAlign: "center"
+  },
+  btn: {
+    fontSize: 20,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 25,
+      padding: 6,
+      paddingLeft: 12,
+      paddingRight: 12,
+      backgroundColor: colors.azulVibrante,
+      borderRadius: 5,
+      color: colors.branco,
+      borderWidth: 1
+  },
+  icon: {
+    height: hp ('1.2%'), 
+    width: hp ('4.9%'),
+    marginBottom: 20
+  }
 })
--- src/screens/TransitionScreens/Apresentacao/ScreenUm.js
+++ src/screens/TransitionScreens/Apresentacao/ScreenUm.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity, Alert } from 'react-native';
-import Header from '../../components/Header';
+import Header from '../../../components/Header';
+import colors from '../../../styles/colors';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 
 export default function ScreenUm(props) {
   return (
     <View style={styles.conatiner}>
-      <Header titulo='Conhecimentos' funcao={() => props.navigation.navigate('Main')} />
+      <Header titulo='' funcao={() => props.navigation.navigate('Main')} />
 
-      <Text style={{ color: "#FFFFFF", fontSize: 30 }}>
-        Conforto e Rapidez
-        </Text>
+      <View style={styles.interno}>
+        <Text style={styles.titulo}>
+          Conforto e Rapidez
+          </Text>
 
-      <Image source={require('../../../../assets/icons/rota.png')}></Image>
+        <Image source={require('../../../../assets/icons/rota.png')}></Image>
 
-      <Text style={{ color: "#FFFFFF", fontSize: 20, textAlign: "center" }}>
-        Solicite um técnico em TI no conforto de sua casa e sem dor de cabeça
-        </Text>
+        <Text style={styles.paragrafo}>
+          Solicite um técnico em TI no conforto de sua casa e sem dor de cabeça
+          </Text>
 
-      <TouchableOpacity style={{ flexDirection: "row" }} onPress={() => props.navigation.navigate('ScreenDois')}>
-        <Text style={{
-          fontSize: 20,
-          justifyContent: "center",
-          alignItems: "center",
-          paddingTop: 25,
-          marginLeft: 30
-        }}>
-          PRÓXIMO
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity  onPress={() => props.navigation.navigate('ScreenDois')}>
+          <Text style={styles.btn}>
+            Próximo
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Image style={styles.icon} source={require('../../../../assets/icons/telaUm.png')}></Image>
     </View>
 
   );
@@ -36,8 +37,41 @@ export default function ScreenUm(props) {
 const styles = StyleSheet.create({
   conatiner: {
     flex: 1,
-    justifyContent: "space-evenly",
     alignItems: "center",
-    backgroundColor: "#0C1C41",
+    backgroundColor: colors.azulEscuro,
   },
+  interno: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly"
+  },
+  titulo: {
+    color: colors.branco,
+    fontSize: 30,
+    textAlign: "center",
+  },
+  paragrafo: {
+    width: wp ('88%'),
+    color: "#FFFFFF", 
+    fontSize: 18,
+    textAlign: "center"
+  },
+  btn: {
+    fontSize: 20,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 25,
+      padding: 6,
+      paddingLeft: 12,
+      paddingRight: 12,
+      backgroundColor: colors.azulVibrante,
+      borderRadius: 5,
+      color: colors.branco,
+      borderWidth: 1
+  },
+  icon: {
+    height: hp ('1.2%'), 
+    width: hp ('4.9%'),
+    marginBottom: 20
+  }
 })
