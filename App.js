fe87d3131066202210675ec9bc9d892b0e156c4b
Dianna-coder <diana270301@hotmail.com>
2020-11-20 12:09:47 -0300

    feat: criar arquivo de rotas

--- App.js
+++ App.js
@@ -1,46 +1,11 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Main from './src/screens/Main';
-import Login from './src/screens/Login';
-import ScreenUm from './src/screens/TransitionScreens/Apresentacao/ScreenUm';
-import ScreenDois from './src/screens/TransitionScreens/Apresentacao/ScreenDois';
-import ScreenTres from './src/screens/TransitionScreens/Apresentacao/ScreenTres';
-import CadastroCliente from './src/screens/Cadastro/CadastroCliente';
-import CadastroTecnico from './src/screens/Cadastro/CadastroTecnico';
-import ProfileCliente from './src/screens/ProfileCliente/ProfileCliente';
-import ProfileProfissional from './src/screens/ProfileProfissional/ProfileProfissional';
-import ResetPassword from './src/screens/ResetPassword';
+import Route from './src/routes/Route';
 
-const Stack = createStackNavigator();
-
-class App extends React.Component {
-
+export default class App extends React.Component {
   render() {
     return (
-      <NavigationContainer>
-        <Stack.Navigator
-
-          screenOptions={{
-            animationEnabled: false,
-            headerShown: false
-          }}
-        >
-          <Stack.Screen name="Main" component={Main} />
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="ScreenUm" component={ScreenUm} />
-          <Stack.Screen name="ScreenDois" component={ScreenDois} />
-          <Stack.Screen name="ScreenTres" component={ScreenTres} />
-          <Stack.Screen name="CadastroCliente" component={CadastroCliente} />
-          <Stack.Screen name="CadastroTecnico" component={CadastroTecnico} />
-          <Stack.Screen name="ProfileCliente" component={ProfileCliente} />
-          <Stack.Screen name="ProfileProfissional" component={ProfileProfissional} />
-          <Stack.Screen name="ResetPassword" component={ResetPassword} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Route />
     );
   }
 }
-
-export default App;
